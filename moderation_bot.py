91f93d92f6a389a5af50b3fe2ea5a4f497b99af3
DAWEDAAW3415 <147543042+DAWEDAAW3415@users.noreply.github.com>
2023-10-10 13:49:09 -0400

    moderation_bot.py

--- moderation_bot.py
+++ moderation_bot.py
@@ -8,7 +8,7 @@ import discord
 
 # Creating client instance - this will be used to interact with the Discord API (connection to Discord)
 client = discord.Client()
-key = "MTE2MTM1MzgzNjA5OTgwMTE2Mg.GY5HOK.MB9sd4dGQdLWU2MzBUYjv2Ts_azNYZfaHTUb3M"
+key = "MTE2MTM1MzgzNjA5OTgwMTE2Mg.GAbruc.OZ23UBDISEltjzKtfarxKsWkf56nSSSnq7DqP0"
 
 # These are all the words you want to block (the last 2 items are so the bot also deletes messages
 # containing links) - You can add as many terms in here as you'd like.
@@ -31,7 +31,6 @@ async def on_message(msg):
             # Checking if the message was sent by a moderator (because it would be nice if moderators
             # could share links in case it would be important for them to do so).
             if "Moderator" not in str(msg.author.roles) and text in str(msg.content.lower()):
-                await msg.author.warn() # Warns Hopefully
                 await msg.delete() # Deletes the message
                 return # So that we don't continue going throuh the loop once we've already found
                        # a blocked word
@@ -39,4 +38,4 @@ async def on_message(msg):
         print("Not Deleting...") # This will be printed if the bot doesn't delete a message
         
 
-client.run(MTE2MTM1MzgzNjA5OTgwMTE2Mg.GY5HOK.MB9sd4dGQdLWU2MzBUYjv2Ts_azNYZfaHTUb3M) # Running the bot"
+client.run(MTE2MTM1MzgzNjA5OTgwMTE2Mg.GAbruc.OZ23UBDISEltjzKtfarxKsWkf56nSSSnq7DqP0) # Running the bot"
